8cfb7dd4deceda7d17d5cec204601bb597592193
niteshctz25 <60354041+niteshctz25@users.noreply.github.com>
2020-07-31 14:43:44 +0530

    Add files via upload

---
+++ length and change case.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+  int c = 0;
+  char ch,s[100];
+  int length;
+
+  printf("Enter a string \n");
+  gets(s);
+
+  length = strlen(s);
+
+  printf("Length of the string = %d\n", length);
+  
+   while (s[c] != '\0') {
+      ch = s[c];
+      if (ch >= 'A' && ch <= 'Z')
+         s[c] = s[c] + 32;
+      else if (ch >= 'a' && ch <= 'z')
+         s[c] = s[c] - 32;  
+      c++;  
+   }
+   
+   printf("Final string is: %s\n", s);
+
+  return 0;
+}
